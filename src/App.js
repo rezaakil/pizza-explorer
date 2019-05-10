f4b48c2f80ff9afc4739ae24cf0690ecc910d410
REZA AKIL <akil.reza@gmail.com>
2019-05-10 09:55:21 +0200

    pizza-add works

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import PizzaListContainer from './components/PizzaListContainer';
+import PizzaDetailsContainer from './components/PizzaDetailsContainer';
+import AddPizzaFormContainer from './components/AddPizzaFormContainer';
 
-function App() {
+
+
+
+class App extends Component {
+  render() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+       
+       <main>
+          < PizzaListContainer />
+          < PizzaDetailsContainer />
+          <AddPizzaFormContainer/>
+
+          
+       </main>
+      </div>
   );
+
+  }
 }
 
 export default App;
